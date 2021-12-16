e99ac76c2839c75a0aa955fa8f605aad7445402b
Eduardo Camacho <eduardocc97@hotmail.com>
2021-12-16 13:01:59 -0600

    Primer Commit

---
+++ ManejoDePresupuesto/Controllers/TiposCuentasController.cs
@@ -0,0 +1,23 @@
+﻿using ManejoDePresupuesto.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManejoDePresupuesto.Controllers
+{
+    public class TiposCuentasController : Controller
+    {
+        public IActionResult Crear()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta tipoCuenta)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(tipoCuenta);
+            }
+            return View();
+
+        }
+    }
+}
---
+++ ManejoDePresupuesto/Models/TipoCuenta.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejoDePresupuesto.Models
+{
+    public class TipoCuenta
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage = "El campo {0} debe tener entre {2} y {1} caracteres")]
+        [Display(Name = "Nombre de tipo de cuenta")]
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }
+    }
+}
